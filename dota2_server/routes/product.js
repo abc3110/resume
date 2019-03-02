1a66c099ee1b9a9b72c953134329a69cba3b814d
abc3110 <670948054@qq.com>
2019-03-02 08:21:33 +0800

    添加/detail接口

--- dota2_server/routes/product.js
+++ dota2_server/routes/product.js
@@ -5,9 +5,21 @@ const pool=require("../pool.js");
 var router=express.Router();
 //获取商品列表：/product/list
 router.get("/list",(req,res)=>{
-	var sql="select * from dota_product";
+	var sql="select * from dota_product"
 	pool.query(sql,(err,result)=>{
 		if(err) throw err;
+		if(result.length>0){
+			res.send({code:1,data:result});
+		}
+	})
+});
+//获取商品信息：/product/detail
+router.get("/detail",(req,res)=>{
+	var id=req.query.id;
+	var sql="select * from dota_product where id=?"
+	pool.query(sql,[id],(err,result)=>{
+		if(err) throw err;
+		console.log(result);
 		if(result.length>0){
 			res.send({code:1,data:result});
 		}
